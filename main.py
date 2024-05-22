e96717a7e425f67bb8ca35706419fcb37b24b98f
MateoBarrera <materile1997@gmail.com>
2024-05-22 17:02:31 -0500

    feat: testing vercel configuration

--- main.py
+++ main.py
@@ -12,7 +12,15 @@ classes of the server, additionally it attends the HTTP requests for the root ad
 from datetime import datetime
 import unittest
 
-from flask import request, make_response, render_template, redirect, session, Response, flash
+from flask import (
+    request,
+    make_response,
+    render_template,
+    redirect,
+    session,
+    Response,
+    flash,
+)
 from app import create_app
 from app.email import send_email
 from flask_login import current_user
@@ -30,7 +38,7 @@ def test():
     """OPTIONAL
     Construction of automatic integration tests
     """
-    tests = unittest.TestLoader().discover('test')
+    tests = unittest.TestLoader().discover("test")
     unittest.TextTestRunner().run(tests)
 
 
@@ -54,14 +62,14 @@ def inject_template_scope():
     injections = dict()
 
     def cookies_check():
-        value = request.cookies.get('cookie_consent')
-        return value == 'true'
+        value = request.cookies.get("cookie_consent")
+        return value == "true"
 
     injections.update(cookies_check=cookies_check)
     return injections
 
 
-@app.route('/')
+@app.route("/")
 def index():
     """
     The index function is the first function that runs when a user visits the website.
@@ -71,8 +79,8 @@ def index():
     :return: A response object that redirects the user to the home page
     """
     user_ip = request.remote_addr
-    response = make_response(redirect('/home'))
-    session['user_ip'] = user_ip
+    response = make_response(redirect("/home"))
+    session["user_ip"] = user_ip
     return response
 
 
@@ -87,7 +95,7 @@ def not_found(error):
     :param error: Pass the error message to the template
     :return: The 400 html template
     """
-    return render_template('404.html', error=error)
+    return render_template("404.html", error=error)
 
 
 @app.errorhandler(500)
@@ -99,10 +107,10 @@ def server_error(error):
     :param error: Pass the error message to the template
     :return: The 500 html template
     """
-    return render_template('500.html', error=error)
+    return render_template("500.html", error=error)
 
 
-@app.route('/home', methods=['GET', 'POST'])
+@app.route("/home", methods=["GET", "POST"])
 def home():
     """
     The home function is the first function that will be called when a user visits the home page.
@@ -110,42 +118,48 @@ def home():
 
     :return: The home page
     """
-    text_module_db = 'En este módulo se recopila la información de las variables y los indicadores de ' \
-                     'eficiencia energética asociados a los procesos de generación, distribución y uso final de la ' \
-                     'energía eléctrica en Colombia, visualizando su tendencia histórica.'
-    text_module_calc = 'En este módulo se presenta la especificación del modelo de proyección para las variables, ' \
-                       'empleadas en la evaluación de estrategias de eficiencia energética del sector eléctrico ' \
-                       'en Colombia. También se incluye la visualización de los resultados de proyección con la ' \
-                       'tendencia histórica de las variables hasta el 2030.'
-    text_module_anl = 'En este módulo se construyen y evalúan las estrategias de eficiencia energética asociadas al ' \
-                      'sector eléctrico colombiano, mediante criterios económicos, ambientales y energéticos, ' \
-                      'permitiendo definir las estrategias más viables para el país, según el año de análisis.'
+    text_module_db = (
+        "En este módulo se recopila la información de las variables y los indicadores de "
+        "eficiencia energética asociados a los procesos de generación, distribución y uso final de la "
+        "energía eléctrica en Colombia, visualizando su tendencia histórica."
+    )
+    text_module_calc = (
+        "En este módulo se presenta la especificación del modelo de proyección para las variables, "
+        "empleadas en la evaluación de estrategias de eficiencia energética del sector eléctrico "
+        "en Colombia. También se incluye la visualización de los resultados de proyección con la "
+        "tendencia histórica de las variables hasta el 2030."
+    )
+    text_module_anl = (
+        "En este módulo se construyen y evalúan las estrategias de eficiencia energética asociadas al "
+        "sector eléctrico colombiano, mediante criterios económicos, ambientales y energéticos, "
+        "permitiendo definir las estrategias más viables para el país, según el año de análisis."
+    )
 
     if current_user.is_authenticated:
-        user_ip = session.get('user_ip')
+        user_ip = session.get("user_ip")
         username = current_user.id
         context = {
-            'user_ip': user_ip,
-            'username': username,
-            'anonymous': False,
-            'text_module_db': text_module_db,
-            'text_module_calc': text_module_calc,
-            'text_module_anl': text_module_anl,
+            "user_ip": user_ip,
+            "username": username,
+            "anonymous": False,
+            "text_module_db": text_module_db,
+            "text_module_calc": text_module_calc,
+            "text_module_anl": text_module_anl,
         }
-        return render_template('module/main.html', **context)
+        return render_template("module/main.html", **context)
     context = {
-        'anonymous': True,
-        'user_ip': "UserIp",
-        'text_module_db': text_module_db,
-        'text_module_calc': text_module_calc,
-        'text_module_anl': text_module_anl,
+        "anonymous": True,
+        "user_ip": "UserIp",
+        "text_module_db": text_module_db,
+        "text_module_calc": text_module_calc,
+        "text_module_anl": text_module_anl,
     }
-    if request.cookies.get('cookie_consent'):
+    if request.cookies.get("cookie_consent"):
         pass
-    return render_template('home/home.html', **context)
+    return render_template("home/home.html", **context)
 
 
-@app.route('/contact', methods=['GET', 'POST'])
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
     """Contact Form method
 
@@ -153,37 +167,37 @@ def contact():
         server response: platform contact form
     """
     context = {
-        'anonymous': True,
-        'user_ip': "UserIp",
+        "anonymous": True,
+        "user_ip": "UserIp",
     }
-    if request.method == 'POST':
+    if request.method == "POST":
         contact_data = request.form.to_dict()
         contact_context = {
-            'mail': contact_data.get('mail'),
-            'name': contact_data.get('name'),
-            'identifier': contact_data.get('identifier', '+57'),
-            'number': contact_data.get('number'),
-            'organization': contact_data.get('organization', 'NA'),
-            'message': contact_data.get('message', 'NA'),
-            'type_contact': 'contacto',
-            'date': datetime.now().strftime("%x"),
-            'hour': datetime.now().strftime("%X")
+            "mail": contact_data.get("mail"),
+            "name": contact_data.get("name"),
+            "identifier": contact_data.get("identifier", "+57"),
+            "number": contact_data.get("number"),
+            "organization": contact_data.get("organization", "NA"),
+            "message": contact_data.get("message", "NA"),
+            "type_contact": "contacto",
+            "date": datetime.now().strftime("%x"),
+            "hour": datetime.now().strftime("%X"),
         }
-        send_email(contact_data.get('mail'), contact_context)
+        send_email(contact_data.get("mail"), contact_context)
         flash("Mensaje enviado")
 
     if current_user.is_authenticated:
-        user_ip = session.get('user_ip')
+        user_ip = session.get("user_ip")
         username = current_user.id
         context = {
-            'user_ip': user_ip,
-            'username': username,
-            'anonymous': False,
+            "user_ip": user_ip,
+            "username": username,
+            "anonymous": False,
         }
-    return render_template('home/contact.html', **context)
+    return render_template("home/contact.html", **context)
 
 
-@app.route('/email', methods=['GET'])
+@app.route("/email", methods=["GET"])
 def email():
     """
     The email function is used to email the admin of the website.
@@ -194,33 +208,36 @@ def email():
     """
 
     context = {
-        'mail': "contact_data.get('mail')",
-        'name': "contact_data.get('name')",
-        'identifier': "contact_data.get('identifier', '+57')",
-        'number': "contact_data.get('number')",
-        'organization': "contact_data.get('organization', 'NA')",
-        'message': "contact_data.get('message', 'NA')",
-        'type_contact': 'contacto',
-        'date': datetime.now().strftime("%x"),
-        'hour': datetime.now().strftime("%X")
+        "mail": "contact_data.get('mail')",
+        "name": "contact_data.get('name')",
+        "identifier": "contact_data.get('identifier', '+57')",
+        "number": "contact_data.get('number')",
+        "organization": "contact_data.get('organization', 'NA')",
+        "message": "contact_data.get('message', 'NA')",
+        "type_contact": "contacto",
+        "date": datetime.now().strftime("%x"),
+        "hour": datetime.now().strftime("%X"),
     }
 
-    return render_template('home/email_template.html', **context)
+    return render_template("home/email_template.html", **context)
 
 
 # DONE
-@app.route('/robots.txt')
+@app.route("/robots.txt")
 def noindex():
     """
     The noindex function is used to restrict the activity of search engine crawlers and stop their access to app.
 
     :return: A text plain response
     """
-    r = Response(response="User-Agent: *\nAllow: /\nAllow: /dashboard/",
-                 status=200, mimetype="text/plain")
+    r = Response(
+        response="User-Agent: *\nAllow: /\nAllow: /dashboard/",
+        status=200,
+        mimetype="text/plain",
+    )
     r.headers["Content-Type"] = "text/plain; charset=utf-8"
     return r
 
 
-if __name__ == '__main__':
-    app.run(debug=True, threaded=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", threaded=True)
